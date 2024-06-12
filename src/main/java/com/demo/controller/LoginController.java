c6cc1be8738c0b46cc6464f35eda2864961edc5a
Janainaacm <janaina.maneiro@gmail.com>
2024-06-12 12:22:06 +0200

    initial commit

---
+++ src/main/java/com/demo/controller/LoginController.java
@@ -0,0 +1,74 @@
+package com.demo.controller;
+
+import com.demo.api.JwtResponse;
+import com.demo.api.LoginRequest;
+import com.demo.repository.RoleRepository;
+import com.demo.repository.UserRepository;
+import com.demo.security.jwt.JwtUtils;
+import com.demo.security.service.UserDetailsImpl;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("/signin")
+public class LoginController {
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    private UserRepository userRepository;
+
+    public LoginController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public String loginView() {
+        return "login";
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, HttpServletResponse response) {
+        Authentication authentication;
+        try {
+            authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.badRequest().body("Invalid username or password");
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        JwtResponse jwtResponse = new JwtResponse(jwt, userDetails.getId(), userDetails.getUsername(), userDetails.getEmail(), roles);
+        return ResponseEntity.ok(jwtResponse);
+    }
+
+
+}
---
+++ src/test/java/com/demo/itsakerhet_grupparbete/ITsakerhetGrupparbeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.itsakerhet_grupparbete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITsakerhetGrupparbeteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
